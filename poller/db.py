7e5e6d706b89633f61d2321ebcb944bad83cd978
Gösta <goesta.o@gmail.com>
2018-08-19 10:58:32 +0200

    fixed database uri

--- poller/db.py
+++ poller/db.py
@@ -5,9 +5,10 @@ db_connection = configuration.get('database')
 
 username = db_connection.get('username')
 password = db_connection.get('password')
+host = db_connection.get('host')
 db_name = db_connection.get('db_name')
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{}:{}@{}:3306/{}".format(username, password, db_name)
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{}:{}@{}:3306/{}".format(username, password, host, db_name)
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 Session = sessionmaker(bind=engine)
 
@@ -17,3 +18,7 @@ def persist(entity):
     session.add(entity)
     session.commit()
     session.close()
+
+
+if __name__ == "__main__":
+    print(SQLALCHEMY_DATABASE_URI)
