42bad75c7e04aa7ac7502b9a962c447a4bdb8760
Gösta <goesta.o@gmail.com>
2018-08-19 11:58:29 +0200

    enhanced model a bit

--- poller/models.py
+++ poller/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, BigInteger, DateTime, Boolean, Float, Sequence, String
+from sqlalchemy import Column, Integer, DateTime, Float, Sequence, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 import datetime
@@ -9,7 +9,7 @@ Base = declarative_base()
 class SensorData(Base):
     __tablename__ = 'sensordata'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    timestamp = Column(DateTime(timezone=True), default=datetime.datetime.now)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
     sensor_name = Column(String(50))
     temperature = Column(Float)
     moisture = Column(Integer)
@@ -21,8 +21,8 @@ class SensorData(Base):
         super().__init__(*args, **kwargs)
 
     def __repr__(self):
-        return '<Timestamp: {0}; temperature: {1}; moisture: {2}; fertility: {3}'.format(self.timestamp,
-                                                                                         self.temperature,
-                                                                                         self.moisture,
-                                                                                         self.fertility)
-
+        return '<Sensor: {4}, Timestamp: {0}; temperature: {1}; moisture: {2}; fertility: {3}>'.format(self.timestamp,
+                                                                                                       self.temperature,
+                                                                                                       self.moisture,
+                                                                                                       self.fertility,
+                                                                                                       self.sensor_name)
